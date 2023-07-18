cb034457e7f252af7a16f7474092d986db4068ad
Amanda Evola <KrustyKrabMan1999@gmail.com>
2023-07-18 18:27:00 -0400

    Add files via upload

---
+++ dateandtime.ps1
@@ -0,0 +1,11 @@
+﻿# Simple PowerShell Greeting Script
+
+# Get the current date and time
+$currentTime = Get-Date
+
+# Get the username of the currently logged-in user
+$currentUserName = $env:USERNAME
+
+# Greet the user and display the current date and time
+Write-Host "Hello, $currentUserName! Welcome to the PowerShell Greeting Script."
+Write-Host "The current date and time is: $currentTime"
